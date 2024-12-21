c779f3e6829da89d60c60ea2f46a7a94579a75b4
Shivansh Vij <shivanshvij@loopholelabs.io>
2024-12-21 18:33:25 -0500

    fixed OG images

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -1,18 +1,20 @@
 import { createMetadataImage } from 'fumadocs-core/server';
 import {architectSource, polyglotSource, scaleSource} from '@/app/source';
 
+const basePath = process.env.NODE_ENV === 'development' ? '/docs' : '';
+
 export const architectMetadataImage = createMetadataImage({
-    imageRoute: '/architect-og',
+    imageRoute: `${basePath}/architect-og`,
 
     source: architectSource,
 });
 
 export const scaleMetadataImage = createMetadataImage({
-    imageRoute: '/scale-og',
+    imageRoute: `${basePath}/scale-og`,
     source: scaleSource,
 });
 
 export const polyglotMetadataImage = createMetadataImage({
-    imageRoute: '/polyglot-og',
+    imageRoute: `${basePath}/polyglot-og`,
     source: polyglotSource,
 });
